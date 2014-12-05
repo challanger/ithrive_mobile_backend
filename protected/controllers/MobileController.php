ba5d6dd1ece6707064496dac19f6ffc448698e47
Aaron Salo <aaron.dune2000@gmail.com>
2014-12-04 21:34:01 -0500

    -added functions that mobile app version 2 uses

--- protected/controllers/MobileController.php
+++ protected/controllers/MobileController.php
@@ -61,6 +61,265 @@ class MobileController extends Controller
            ));*/
     }
     
+    public function actionSlides()
+    {
+        if((isset($_GET["callback"]))&&($_GET["callback"]!=""))
+            $callBack = $_GET["callback"];
+        else 
+            $callBack = "jsonCallback";
+        echo "$callBack(" .json_encode($this->loadActiveSlides()).")";
+    }
+    
+    public function actionMedia()
+    {
+        $send = array();
+        $send["media"]=$this->loadActiveMedia();
+        $send["messages"]=$this->loadActiveMessage();
+        
+        if((isset($_GET["callback"]))&&($_GET["callback"]!=""))
+            $callBack = $_GET["callback"];
+        else 
+            $callBack = "jsonCallback";
+        
+        echo "$callBack(" .json_encode($send).")";
+    }
+    
+    public function actionMediaAll()
+    {
+        $send = $this->loadActiveMediaAll();
+        
+        if((isset($_GET["callback"]))&&($_GET["callback"]!=""))
+            $callBack = $_GET["callback"];
+        else 
+            $callBack = "jsonCallback";
+        
+        echo "$callBack(" .json_encode($send).")";
+    }
+    
+    public function actionMessageDetail($id)
+    {
+        $send = array();
+        $send["message"] = $this->loadMessageByID($id);
+        if(($send["message"]!=array()) && ($send["message"]["catID"]))
+            $send["category"] = $this->loadCategory($send["message"]["catID"]);
+        else 
+            $send["category"] = array();
+        
+        if((isset($_GET["callback"]))&&($_GET["callback"]!=""))
+            $callBack = $_GET["callback"];
+        else 
+            $callBack = "jsonCallback";
+        
+        echo "$callBack(" .json_encode($send).")";
+    }
+    /**
+     * pull all the active slides
+     * @return array
+     */
+    private function loadActiveSlides()
+    {
+        $time = time();
+        $criteria = new CDbCriteria;
+        $criteria->condition='active=:active AND starts<:dateCode AND expires>:dateCodee';
+        $criteria->params=array(':active'=>1,':dateCode'=>$time,':dateCodee'=>$time);
+        $slides=  Slides::model()->findAll($criteria);
+        //$slides=  Slides::model()->findAll('active=:active AND starts>:dateCode AND expires<:dateCodee',array(':active'=>1,':dateCode'=>$time,':dateCodee'=>$time));
+        
+        if($slides===null)
+            return array();
+        else
+        {
+            $send_slides=array();
+            foreach($slides as $slide)
+            {
+                $send_slide=array('id'=>$slide->id,
+                    'imageurl'=>$slide->imageurl,
+                    'link'=>$slide->link,
+                    'caption'=>$slide->caption,
+                    'start_date'=>$slide->starts,
+                    'expires'=>$slide->expires,
+                    'active'=>$slide->active,
+                    'last_modified'=>$slide->updated);
+                if($slide->imageurl=="")
+                    $send_slide['imageurl']=Yii::app()->getBaseUrl(true) . "/images/slide_default_260_250.png"; 
+                array_push($send_slides, $send_slide);
+            }
+            return $send_slides;
+        }
+    }
+    
+    /**
+     * pull all the active media
+     * @return array
+     */
+    private function loadActiveMedia()
+    {
+        $time = time();
+        $criteria=new CDbCriteria(); 
+        $criteria->order="date";
+        $criteria->condition='active=:active';
+        $criteria->params=array(':active'=>1);
+        $messageCategories=  Category::model()->findAll($criteria);
+        
+        if($messageCategories===null)
+        {
+            return array();
+        }
+        else 
+        {
+            $send_media=array(); 
+            foreach($messageCategories as $category)
+            {
+                $send_category=array("id"=>$category->id,
+                    'imageurl'=>$category->imageurl,
+                    'title'=>$category->title,
+                    'author'=>$category->author,
+                    'date'=>$category->date,
+                    'active'=>$category->active,
+                    'last_modified'=>$category->updated);
+                if($category->imageurl=="")
+                    $send_category['imageurl']=Yii::app()->getBaseUrl(true) . "/images/message_category_default_100_100.png"; 
+                    array_push($send_media, $send_category);
+            }
+            return $send_media;
+        }
+    }
+    
+    /**
+     * pull all the active media Categories and messages 
+     * @return array
+     */
+    private function loadActiveMediaAll()
+    {
+        $time = time();
+        $criteria=new CDbCriteria(); 
+        $criteria->order="date";
+        $criteria->condition='active=:active';
+        $criteria->params=array(':active'=>1);
+        $messageCategories=  Category::model()->findAll($criteria);
+        
+        if($messageCategories===null)
+        {
+            return array();
+        }
+        else 
+        {
+            $send_media=array(); 
+            foreach($messageCategories as $category)
+            {
+                $send_category=array("id"=>$category->id,
+                    'imageurl'=>$category->imageurl,
+                    'title'=>$category->title,
+                    'author'=>$category->author,
+                    'date'=>$category->date,
+                    'active'=>$category->active,
+                    'last_modified'=>$category->updated);
+                if($category->imageurl=="")
+                    $send_category['imageurl']=Yii::app()->getBaseUrl(true) . "/images/message_category_default_100_100.png"; 
+                       
+                $send_category["messages"] = $this->loadActiveMessage($category->id);
+                array_push($send_media, $send_category);
+            }
+            return $send_media;
+        }
+    }
+    
+    /**
+     * pull all the active media
+     * @return array
+     */
+    private function loadCategory($id)
+    {
+        $category=  Category::model()->findByPk($id);
+        
+        if($category===null)
+        {
+            return array();
+        }
+        else 
+        {
+            $send_category=array("id"=>$category->id,
+                    'imageurl'=>$category->imageurl,
+                    'title'=>$category->title,
+                    'author'=>$category->author,
+                    'date'=>$category->date,
+                    'active'=>$category->active,
+                    'last_modified'=>$category->updated);
+                if($category->imageurl=="")
+                    $send_category['imageurl']=Yii::app()->getBaseUrl(true) . "/images/message_category_default_100_100.png"; 
+            
+            return $send_category;
+        }
+    }
+    
+    /**
+     * pull all the active messages
+     * @return array
+     */
+    private function loadActiveMessage($category=0)
+    {
+        $time = time();
+        $criteria=new CDbCriteria(); 
+        $criteria->order="date";
+        if($category !== 0)
+        {
+            $criteria->condition='active=:active AND category=:category';
+            $criteria->params=array(':active'=>1,':category'=>$category);
+        }
+        else 
+        {
+            $criteria->condition='active=:active';
+            $criteria->params=array(':active'=>1);
+        }
+        $messages=  Message::model()->findAll($criteria);
+        
+        if($messages===null)
+        {
+            return array();
+        }
+        else 
+        {
+            $send_messages=array();
+            foreach($messages as $message)
+            {
+                $send_message=array('id'=>$message->id,
+                    'catID'=>$message->category,
+                    'url'=>$message->url,
+                    'title'=>$message->title,
+                    'date'=>$message->date,
+                    'active'=>$message->active,
+                    'last_modified'=>$message->updated);
+                array_push($send_messages, $send_message);
+            }
+            return $send_messages;
+        }
+    }
+    
+    /**
+     * pull a given message
+     * @return array
+     */
+    private function loadMessageByID($id)
+    {
+        $message=  Message::model()->findByPK($id);
+        
+        if($message===null)
+        {
+            return array();
+        }
+        else 
+        {
+                $send_message=array('id'=>$message->id,
+                    'catID'=>$message->category,
+                    'url'=>$message->url,
+                    'title'=>$message->title,
+                    'date'=>$message->date,
+                    'active'=>$message->active,
+                    'last_modified'=>$message->updated);
+            return $send_message;
+        }
+    }
+    
     /**
      * pull all the slides that are active
      * @return array
